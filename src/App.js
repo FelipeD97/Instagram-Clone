469d5caf355d6473f743b77c1aeef69a592f5d0d
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2020-09-03 18:51:12 -0400

    Connected firebase to store data.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Post from "./Post";
+import { db } from "./firebase";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username: "felipe_aalejandro",
-      caption: "WOW it works",
-      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQqLeHpuAUmTKMdcTWLevz1elcwImSMZKn8Ww&usqp=CAU"
-    },
-    {
-      username: "felipe_aalejandro",
-      caption: "WOW it works",
-      imageUrl: "https://images.unsplash.com/photo-1597680460531-60a68dfbf6cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-    }
-  ]);
+  const [posts, setPosts] = useState([]);
+
+  // UseEffect: Runs a piece of code based on a specific condition
+  // 
+  
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
+    })
+  }, []);
 
   return (
     <div className="app">
@@ -29,14 +31,10 @@ function App() {
       <h1>Hello! Lets create an Instagram Clone</h1>
 
       {
-        posts.map(post => (
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        posts.map(({id, post}) => (
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
         ))
       }
-
-      <Post username="felipe_aalejandro" caption="WOW it works" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQqLeHpuAUmTKMdcTWLevz1elcwImSMZKn8Ww&usqp=CAU"/>
-      <Post username="theycallme_lipe" caption="I love this place" imageUrl="https://images.unsplash.com/photo-1597764279120-f1e53073301d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
-      <Post username="yvng_lipe" caption="Love my dog" imageUrl="https://images.unsplash.com/photo-1597784156691-8388123e5d84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
     </div>
   );
 }
